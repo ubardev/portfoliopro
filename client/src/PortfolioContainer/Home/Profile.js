c8c7ece74d0216ba6f231407c11076b466b7df17
ubardev <bellin83@gmail.com>
2021-12-31 12:29:40 +0900

    :sparkles: Add: 간략 소개 추가
    Typical 이용해서 타이핑 치는 효과

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Typical from 'react-typical';
 
 const Profile = (props) => {
   return (
@@ -11,6 +12,43 @@ const Profile = (props) => {
             </a>
             <a href="https://blog.ubar.kr" target="_blank">
               <i className="fa fa-rss"></i>
+            </a>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {' '}
+              안녕하세요. <span className="highlighted-text">황종인</span>입니다.{' '}
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              <h1>
+                {' '}
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    'Frontend developer 🖥️',
+                    1000,
+                    'Backend developer ☁️',
+                    1000,
+                    'Team leader 👑',
+                    1000,
+                    'JavaScript, React, Augular.js 😎',
+                    1000,
+                    'Java, Spring boot, Node.js, PHP 🧐',
+                    1000,
+                  ]}
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                JANDI에서 프론트엔드 팀 리딩을 하고 있습니다.
+              </span>
+            </span>
+          </div>
+          <div className="profile-optins">
+            <button className="btn primary-btn"> Contact Me </button>
+            <a href="#" download="Hwang22.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
             </a>
           </div>
         </div>
